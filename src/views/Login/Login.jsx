b23e31d329bbc34a7185c0e72d9f9aea903af224
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-05-05 15:10:54 -0700

    updated

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
 import { NavLink, useNavigate } from "react-router-dom";
 import axios from "axios";
 import useLogin from "../../Hooks/useLogin";
-
+import Loader from '../../components/Loader/Loader'
+import { notification } from "antd";
 const Login = () => {
-  const { login, handleChange } = useLogin();
-
+  const { login, handleChange, loading, ErrorMessage } = useLogin();
   return (
     <>
+      {ErrorMessage && notification.error({
+        message: "Error",
+        description: ErrorMessage,
+        placement: "topRight",
+      })
+      }
       <h1 className="text-[26px] font-[500] mb-2 ">Login</h1>
       <p className="text-[17px] text-[#4c4c4c] mb-8 ">
         Access to our dashboard
@@ -21,9 +27,13 @@ const Login = () => {
           type="text"
           placeholder="Phone Number"
         />
-        <Input onChange={handleChange} name="password" type="password" />
+        <Input
+
+          onChange={handleChange}
+          name="password"
+          type="password" />
         <Button type="primary" className="w-full flex">
-          Login
+          {loading ? <Loader /> : "Login"}
         </Button>
       </form>
 
